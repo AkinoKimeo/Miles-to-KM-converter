b2106749686d80485702abf1f4b2cfca869547b8
AkinoKimeo <47053804+AkinoKimeo@users.noreply.github.com>
2023-06-07 03:26:26 +0000

    Commit one lol
    a program that converts Miles to Kilometers

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
+//This program converts Miles to Kilometers.
 #include <stdio.h>
+#define KMS_PER_MILE 1.609
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int
+main(void)
+{
+  double miles, kms;
+  //distance in miles equivalent distance in kilometers
+  /* get the distance in miles */
+  printf("Enter the distance in miles > ");
+  scanf("%lf", &miles);
+
+  /*convert the distance to kilometers. */
+  kms = KMS_PER_MILE * miles;
+
+  /*Display the distance in kiometers. */
+  printf("That equals %f in kilometers. \n", kms);
+
+  return (0);
+
 }
